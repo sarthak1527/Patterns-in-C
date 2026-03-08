c1c6097e37b08e18411b4f61e348e914d913eb9c
Sarthak Acharya <sarthakacharya1527@gmail.com>
2026-03-08 18:31:25 +0545

    Create diamond.c

---
+++ diamond.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    /* Upper half */
+    for (int i = 1; i <= n; i++) {
+
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (int j = 1; j <= 2*i - 1; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    /* Lower half */
+    for (int i = n - 1; i >= 1; i--) {
+
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (int j = 1; j <= 2*i - 1; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
