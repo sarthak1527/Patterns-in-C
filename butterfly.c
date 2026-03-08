34f295270a4535a29ac016715f0c424122fdd2a7
Sarthak Acharya <sarthakacharya1527@gmail.com>
2026-03-08 18:30:34 +0545

    Create butterfly.c

---
+++ butterfly.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    /* Upper half */
+    for(int i = 1; i <= n; i++) {
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        for(int j = 1; j <= 2*(n-i); j++)
+            printf(" ");
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    /* Lower half */
+    for(int i = n; i >= 1; i--) {
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        for(int j = 1; j <= 2*(n-i); j++)
+            printf(" ");
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
